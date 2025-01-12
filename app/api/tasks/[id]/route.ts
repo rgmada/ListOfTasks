4f706f77b59ce8dd2fa71a6ca75f320f95f4206c
Madalina Reut-Gafituc <reutgafitucmadalina@gmail.com>
2025-01-12 13:27:34 +0200

    Fixed react version and removed react-beautiful-dnd

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,32 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/Utils/connect";
 import { auth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+
 
 export async function DELETE(
-    req: Request, 
-    { params }: { params: { id: string } }
-){
-    try {
+  req: NextRequest,  
+  { params }: { params: { id: string } }  
+) {
+  try {
+    const { userId } = await auth();
 
-        const { userId } = await auth();
+    const { id } = params;
 
-        const { id } = params;
-
-        if (!userId) {
-            return NextResponse.json({ error: "Unauthorized", status: 401 });
-        }
-
-        const task = await prisma.task.delete({
-            where: {
-                id,
-            },
-        });
-
-        console.log("TASK DELETED: ", task);
-        return NextResponse.json(task);
-        
-    } catch (error) {
-        console.log("ERROR DELETING TASK: ", error);
-        return NextResponse.json ({error: "Error deleting task", status: 500});
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized", status: 401 });
     }
+
+    const task = await prisma.task.delete({
+      where: {
+        id,
+      },
+    });
+
+    console.log("TASK DELETED: ", task);
+    return NextResponse.json(task);
+  } catch (error) {
+    console.log("ERROR DELETING TASK: ", error);
+    return NextResponse.json({ error: "Error deleting task", status: 500 });
+  }
 }
