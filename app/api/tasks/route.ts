d8591d45dfd758493485f3533594a7c832df8648
Madalina Reut-Gafituc <reutgafitucmadalina@gmail.com>
2025-01-13 21:56:33 +0200

    Resolved unused variables

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/app/Utils/connect";
