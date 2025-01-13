43893a3bd36595d30972e77c5438c1bef91a64fc
Madalina Reut-Gafituc <reutgafitucmadalina@gmail.com>
2025-01-13 22:51:19 +0200

    Resolved params variable DELETE

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,17 +1,15 @@
-import { NextRequest, NextResponse } from "next/server"; 
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/Utils/connect";
 import { auth } from "@clerk/nextjs/server";
 
-
 export async function DELETE(
-  // @typescript-eslint/no-unused-vars
-  _req: NextRequest,  
-  { params }: { params: { id: string } }  
+  req: NextRequest,
+  { params }: { params: { [key: string]: string | string[] } } // Updated type
 ) {
   try {
     const { userId } = await auth();
 
-    const { id } = params;
+    const id = params.id as string; // Safely cast to string
 
     if (!userId) {
       return NextResponse.json({ error: "Unauthorized", status: 401 });
